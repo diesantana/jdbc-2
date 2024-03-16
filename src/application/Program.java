c52615bccb7080d2bc39dd5d3f70de291e96a3dd
Diego Santana <3c.santana@gmail.com>
2024-03-16 16:07:03 -0300

    Realizando um  consulta pelo JDBC e imprimindo no console

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,37 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Connection conn = DB.connectDB();
-		DB.closeConnection();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet result = null;
+		
+		try {
+			conn = DB.connectDB();
+			st = conn.createStatement();
+			result = st.executeQuery("SELECT * FROM department");
+			
+			while(result.next()) {
+				System.out.println(result.getInt("Id") +", " + result.getString("Name"));
+			}
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeConnection();
+			DB.closeStatement(st);
+			DB.closeResultSet(result);
+		}
 
 	}
 
